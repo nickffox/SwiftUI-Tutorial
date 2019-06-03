51abf67a699d9dc807d5a034f745c7bf2291d8a9
Nick Fox <nicholas.f.fox@gmail.com>
2019-06-03 16:57:25 -0400

    step3

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Nick Fox on 6/3/19.
+//  Copyright © 2019 Nick Fox. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
