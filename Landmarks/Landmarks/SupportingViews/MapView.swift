dbadfae3c083ad06f2e1aeca3cb165e2c4e3fa04
Nick <nick@livesafemobile.com>
2019-06-04 00:09:57 -0400

    wip

--- Landmarks/Landmarks/SupportingViews/MapView.swift
+++ Landmarks/Landmarks/SupportingViews/MapView.swift
@@ -11,12 +11,13 @@ import MapKit
 
 struct MapView : UIViewRepresentable {
 
+    var coordinate: CLLocationCoordinate2D
+
     func makeUIView(context: Context) -> MKMapView {
         MKMapView(frame: .zero)
     }
 
     func updateUIView(_ view: MKMapView, context: Context) {
-        let coordinate = CLLocationCoordinate2DMake(34.011286, -116.166868)
         let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         view.setRegion(region, animated: true)
@@ -26,7 +27,7 @@ struct MapView : UIViewRepresentable {
 #if DEBUG
 struct MapView_Previews : PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: landmarkData[0].locationCoordinate)
     }
 }
 #endif
