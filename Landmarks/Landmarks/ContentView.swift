50fb8bcc605290ec229e84abc7579fe3d80c8d0f
Nick Fox <nick@livesafemobile.com>
2019-06-03 22:34:56 -0400

    Creating and Combining Views Tutorial (#1)
    https://developer.apple.com/tutorials/swiftui/creating-and-combining-views

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Nick Fox on 6/3/19.
+//  Copyright © 2019 Nick Fox. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+
+                    Spacer()
+
+                    Text("California")
+                        .font(.subheadline)
+
+                }
+            }
+            .padding()
+
+            Spacer()
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
